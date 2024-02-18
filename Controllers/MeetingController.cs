76b65b5d35508028c2e47141e41d9e8c2dc8f02e
cihatkama <cihatkama@gmail.com>
2024-02-18 21:13:32 +0300

    Model binding yapıldı, gitignore eklendi.

--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -4,17 +4,26 @@ namespace MeetingApp.Controllers
 {
     public class MeetingController : Controller
     {
+        [HttpGet]   // bir action metodunun HttpVerb tipi seçilmezse default olarak HttpGet olarak işaretlenir.
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Apply() 
+        public IActionResult Apply()
         {
-            return View();   
+            return View();
         }
 
-        public IActionResult List() 
+        [HttpPost]
+        public IActionResult Apply(string Name, string Email, string Phone, bool WillAttend)
+        {
+            // Model Binding aracılığı ile değerler arayüzden post edilir.
+            Console.WriteLine(Name + " - " + Email + " - " + Phone + " - " + WillAttend.ToString());
+            return View();
+        }
+
+        public IActionResult List()
         {
             return View();
         }
